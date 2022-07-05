0998f552c3599ac4ff20ace101f612d8fdeca808
DESKTOP-MP8KBST <sadiqahmed0812@gmail.com>
2022-07-06 00:22:20 +0530

    updated with parameters annotations

--- practice1/src/test/java/practice1/login.java
+++ practice1/src/test/java/practice1/login.java
@@ -1,6 +1,8 @@
 package practice1;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -8,8 +10,9 @@ import org.testng.annotations.Test;
 public class login {
 	
 	@Test
+	@Parameters({"mail","pswrd"})
 	
-	public void pagelogin()
+	public void pagelogin(String mail,String pswrd)
 	{
 		
 		System.setProperty("webdriver.chrome.driver","E:\\Automation_Selenium\\Libraries\\chromedriver_win32_103\\chromedriver.exe");
@@ -19,7 +22,30 @@ public class login {
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		
-		driver.get("https://www.flipkart.com");
+		driver.get("https://www.freecrm.com/");
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+				
+		String pagetitle = driver.getTitle();	
+		System.out.println(pagetitle);
+		
+		
+		driver.findElement(By.xpath("//a[contains(text(),'Login')]")).click();
+		try {
+			Thread.sleep(15000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		driver.findElement(By.xpath("//input[@name='email']")).sendKeys(mail);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(pswrd);
+		driver.findElement(By.xpath("//div[@class='ui fluid large blue submit button']")).click();
+		
 		try {
 			Thread.sleep(3000);
 		} catch (InterruptedException e) {
@@ -27,10 +53,7 @@ public class login {
 			e.printStackTrace();
 		}
 		
-		String pagetitle = driver.getTitle();
 		
-		
-		System.out.println(pagetitle);
 		
 		driver.close();
 		
